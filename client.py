4a5651dc61747d70a4dce0aa08ee4908536ecdd9
MarkusRedluxan <MarkusRedluxan@hotmail.com>
2019-12-12 23:37:18 -0400

    client script division fixed

--- client.py
+++ client.py
@@ -12,8 +12,23 @@ help_msg = {
 	'clear': ['', 'Clear all lines.']
 }
 
+done = False
 name = ''
 server = {}
+client = None
+
+def exit():
+	global done, client
+	done = False
+	client.close()
+
+func = {
+	'exit': close,
+	'help': man,
+	'list': lst,
+	'send': send,
+	'clear': clear
+}
 
 def login():
 	global name, server
@@ -32,6 +47,8 @@ def login():
 		server['port'] = 6666
 
 def setup():
+	global client, server
+
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	client.connect((server['ip'], server['port']))
 
@@ -43,61 +60,67 @@ def setup():
 
 	print('OK!\n')
 
-def main():
-	while True:
-		cmd = input('%s@%s: ' % (name, server['name'])).strip().split(' ')
-		cmd[0] = cmd[0].lower()
-	
-		print()
-	
-		if not cmd[0]:
-			os.system('cls' if os.name == 'nt' else 'clear')
-			client.send(str.encode('list:msgs'))
+def check():
+	cmd = input('%s@%s: ' % (name, server['name'])).strip().split(' ')
+	cmd[0] = cmd[0].lower()
+
+	print()
+
+	if not cmd[0]:
+		os.system('cls' if os.name == 'nt' else 'clear')
+		client.send(str.encode('list:msgs'))
+		reply = client.recv(2048).decode()
+		print(reply)
+
+	elif cmd[0] == 'exit':
+		exit()
+
+	elif cmd[0] == 'help':
+		if len(cmd) == 1:
+			for msg in help_msg:
+				print('%s %s: %s' % (msg, help_msg[msg][0], help_msg[msg][1]))
+
+			print()
+
+		else:
+			try:
+				print('%s %s: %s\n' % (cmd[1], help_msg[cmd[1]][0], help_msg[cmd[1]][1]))
+			except KeyError:
+				print('# Unknown command!\n')
+
+	elif cmd[0] == 'list':
+		if len(cmd) == 1:
+			print('# What do you want to list?')
+			print('Tip: Use \'help list\'.\n')
+
+		else:
+			client.send(str.encode('list:' + cmd[1].lower()))
 			reply = client.recv(2048).decode()
 			print(reply)
-		
-		elif cmd[0] == 'exit':
-			break
-	
-		elif cmd[0] == 'help':
-			if len(cmd) == 1:
-				for msg in help_msg:
-					print('%s %s: %s' % (msg, help_msg[msg][0], help_msg[msg][1]))
-	
-				print()
-	
-			else:
-				try:
-					print('%s %s: %s\n' % (cmd[1], help_msg[cmd[1]][0], help_msg[cmd[1]][1]))
-				except KeyError:
-					print('# Unknown command!\n')
-	
-		elif cmd[0] == 'list':
-			if len(cmd) == 1:
-				print('# What do you want to list?')
-				print('Tip: Use \'help list\'.\n')
-	
-			else:
-				client.send(str.encode('list:' + cmd[1].lower()))
-				reply = client.recv(2048).decode()
-				print(reply)
-	
-		elif cmd[0] == 'send' or cmd[0] == '-':
-			if len(cmd) == 1:
-				print('# You cannot send a empty message!')
-				print('Tip: Use \'help send\'.\n')
-	
-			else:
-				client.send(str.encode('add:' + ' '.join(cmd[1:])))
-				reply = client.recv(2048).decode()
-	
-		elif cmd[0] == 'clear' or cmd[0] == 'cls':
-			os.system('cls' if os.name == 'nt' else 'clear')
-	
+
+	elif cmd[0] == 'send' or cmd[0] == '-':
+		if len(cmd) == 1:
+			print('# You cannot send a empty message!')
+			print('Tip: Use \'help send\'.\n')
+
 		else:
-			print('# Unknown command!\n')
+			client.send(str.encode('add:' + ' '.join(cmd[1:])))
+			reply = client.recv(2048).decode()
+
+	elif cmd[0] == 'clear' or cmd[0] == 'cls':
+		os.system('cls' if os.name == 'nt' else 'clear')
+
+	else:
+		print('# Unknown command!\n')
+
+def main():
+	global done
 	
-	client.close()
+	login()
+	seput()
+	
+	while not done:
+		check()
 
 if __name__ == '__main__':
 	main()
